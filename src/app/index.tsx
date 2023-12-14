ef46599df22794bf41c611dbb623754ae93b3953
chaedir dwiantara <chaedirdwiantara@chaedirs-MacBook-Pro.local>
2023-12-14 19:56:09 +0700

    Setup Expo Router and the main screens

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import {
-  ActivityIndicator,
-  FlatList,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
-import DayListItem from "./src/components/core/DayListItem";
+import { FlatList, StyleSheet, View } from "react-native";
 import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
 import {
   AmaticSC_400Regular,
@@ -14,12 +7,13 @@ import {
 } from "@expo-google-fonts/amatic-sc";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
+import DayListItem from "../components/core/DayListItem";
 
 SplashScreen.preventAutoHideAsync();
 
 const days = [...Array(24)].map((val, index) => index + 1);
 
-export default function App() {
+export default function HomeScreen() {
   let [fontsLoaded, fontError] = useFonts({
     Inter: Inter_900Black,
     Amatic: AmaticSC_400Regular,
