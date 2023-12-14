d489ca01ed1bbd4635414f8d023ddd8e81ad04e3
chaedir dwiantara <chaedirdwiantara@chaedirs-MacBook-Pro.local>
2023-12-14 20:18:41 +0700

    loads font in root layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,30 @@
 import { Slot, Stack } from "expo-router";
+import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
+import {
+  AmaticSC_400Regular,
+  AmaticSC_700Bold,
+} from "@expo-google-fonts/amatic-sc";
+import { useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  let [fontsLoaded, fontError] = useFonts({
+    Inter: Inter_900Black,
+    Amatic: AmaticSC_400Regular,
+    AmaticBold: AmaticSC_700Bold,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: "DEVember" }} />
